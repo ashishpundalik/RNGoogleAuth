e8d1eb25e262dbc341aec651fc86272294b9d233
Aashish Pundalik <pundalikaashish@gmail.com>
2018-02-19 11:45:15 +0800

    Added Google Auth for ios

---
+++ App.js
@@ -0,0 +1,129 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback, Text } from 'react-native';
+import { GoogleSignin } from 'react-native-google-signin';
+
+console.ignoredYellowBox = ['Warning: ReactNative.createElement'];
+
+type Props = {};
+
+export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isUserLoggedIn: false,
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    this.setupGoogleAuth();
+  }
+
+  signInWithGoogle() {
+    GoogleSignin.signIn()
+      .then((user) => {
+        console.log(user);
+        this.setState({
+          isUserLoggedIn: true,
+          user
+        });
+      })
+      .catch((err) => {
+        console.log('WRONG SIGNIN', err);
+      })
+      .done();
+  }
+
+  async setupGoogleAuth() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true });
+      await GoogleSignin.configure({
+        iosClientId: '851068558963-jqtq3q6mk7vpjvkmd348nauqbpu8589n.apps.googleusercontent.com',
+        webClientId: '851068558963-9s6b725j9q0vv8dso6ejr0mb0k8kecld.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+      const user = await GoogleSignin.currentUserAsync();
+      if(user !== null && user.id !== null) {
+        this.setState({
+          isUserLoggedIn: true,
+          user
+        });
+      }
+      console.log(user);
+    }
+    catch (err) {
+      console.log("Google signin error", err.code, err.message);
+    }
+  }
+
+  signOutFromGoogle() {
+    GoogleSignin.signOut()
+      .then(() => {
+        console.log(`User successfully signed out`);
+        this.setState({
+          isUserLoggedIn: false,
+          user: null
+        });
+      })
+      .catch((err) => {
+        console.log('Could not signout', err);
+      });
+  }
+
+  render() {
+    console.log("Rendering with state: ", this.state);
+    return (
+      <View style={ styles.container }>
+        { this.state.isUserLoggedIn ?
+          <Text style={ styles.loggedInText }>
+            Logged In as { `${this.state.user.givenName} ${this.state.user.familyName}` }
+          </Text> :
+          <TouchableWithoutFeedback onPress={this.signInWithGoogle.bind(this)}>
+            <View style={ styles.button }>
+              <Text style={ styles.buttonText }>Login With Google</Text>
+            </View>
+          </TouchableWithoutFeedback>
+        }
+        <TouchableWithoutFeedback onPress={this.signOutFromGoogle.bind(this)}>
+          <View style={ styles.button }>
+            <Text style={ styles.buttonText }>Signout from Google</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  button: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 16,
+    borderRadius: 5,
+    marginTop: 20,
+    minWidth: 180
+  },
+  loggedInText: {
+    fontWeight: 'bold'
+  },
+  buttonText: {
+    textAlign: 'center'
+  }
+});
---
+++ ios/Pods/Target Support Files/Pods-RNGoogleAuth/Pods-RNGoogleAuth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNGoogleAuth : NSObject
+@end
+@implementation PodsDummy_Pods_RNGoogleAuth
+@end
